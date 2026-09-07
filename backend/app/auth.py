49916d9ff8ee3e3eaefd65dbc9376decc0674e85
yashvardhanshah <yashvadhan.harshad2023@vitstudent.ac.in>
2026-09-07 17:39:16 +0530

    Add JWT token creation and verification

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,4 +1,11 @@
+
+import os
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
 from passlib.context import CryptContext
+from dotenv import load_dotenv
+
+load_dotenv()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -8,3 +15,25 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+#JWT
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24  # 1 day
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+    return encoded_jwt
+
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
+        return payload
+    except JWTError:
+        return None
--- backend/test_auth.py
+++ backend/test_auth.py
@@ -5,3 +5,15 @@ print("Hashed:", hashed)
 
 print("Correct password check:", verify_password("mydog123", hashed))
 print("Wrong password check:", verify_password("wrongpassword", hashed))
+
+#JWT Tokens part
+from app.auth import create_access_token, decode_access_token
+
+token = create_access_token({"user_id": 1})
+print("Token:", token)
+
+decoded = decode_access_token(token)
+print("Decoded:", decoded)
+
+fake_decoded = decode_access_token(token + "tampered")
+print("Tampered token result:", fake_decoded)
